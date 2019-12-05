97db096429aadeb3d010a8659d2e228a3d7a69dd
AH228589 <AH228589@falmouth.ac.uk>
2019-12-05 09:41:04 +0000

    Structure should be fixed so the gitignore works. Added PlayerMove and PlayerLook scripts. Added a few useful materials.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //Inputs and movement speed, should be touched only in the unity editor.
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    //Settings for the jump, the important part of this is in the unity editor.
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+    private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+        JumpInput();
+        if ((horizInput != 0 || vertInput != 0) && !isJumping)
+        {
+            //Walking
+        }
+
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        //Calculations for the jumping event, setting the slope to 90 to avoid clipping once in the air.
+        do
+        {
+            //jumpforce is based on the jumpfalloff pattern, set up in the unity editor while evaluating the time in the air.
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above); //Makes sure the player can only jump if he's grounded and there's no collision flags above him.
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+}
