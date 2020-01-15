4206208eec73840866331bfd84496a936bb573bf
Bogdanoff <ChovekaZavur@gmail.com>
2020-01-15 23:46:06 +0000

    - Added Photon Unity Networking - Basic Lobby System (Join a random room, if none exist, create a new one) - Syncing Characters on all screens
    //Check NetworkManager.cs for basic code

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Spawn();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join random room.");
+        PhotonNetwork.CreateRoom("New Room", new Photon.Realtime.RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 5 });
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room with name exists.");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Nice.");
+    }
+
+    public void Spawn()
+    {
+        GameObject currentPlayer = (GameObject)PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+        currentPlayer.GetComponentInChildren<PlayerMove>().enabled = true;
+        currentPlayer.transform.Find("Camera").gameObject.SetActive(true);
+    }
+}
