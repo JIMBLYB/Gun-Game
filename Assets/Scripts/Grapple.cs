fe9cd56dd0c6c9ad87f01e421055912bbbcd2f64
James <git@jamesberry.uk>
2020-02-07 10:34:05 +0000

    Reset player variables and fixed bug where player can 'slingshot' past grapple point

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -67,7 +67,6 @@ public class Grapple : MonoBehaviour
 
         if (Input.GetKeyDown(reelInKey) && grappleFired)
         {
-            reelDestination = hit.point - plugWire.man.position;
             reelIE = StartCoroutine(WirePull());
         }
     }
@@ -103,6 +102,7 @@ public class Grapple : MonoBehaviour
 
         if (plugWire.man.transform.position != hit.point)
         {
+            reelDestination = hit.point - plugWire.man.position;
             plugWire.man.GetComponent<CharacterController>().Move(reelDestination / reelSpeedDivisor);
         }
 
